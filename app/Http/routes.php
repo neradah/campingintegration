c02b0cf9cd0db955de226d55b84f197b7faa2433
richiej053 <richiej053@gmail.com>
2015-12-07 10:54:56 +0000

    Neo Admin

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,8 +2,12 @@
 
 Route::get('/', 'PageController@getHome');
 
+
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
 
+    Route::get('/home', function(){
+        return view('admin.home');
+    });
     Route::resource('event', 'EventController');
 
 });
